5a3f30f489b17bbc1548c69b9bf6d3fc2982c0cb
vrmaverick <119831329+vrmaverick@users.noreply.github.com>
2024-01-28 22:25:32 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,28 @@
+from google.colab import drive
+drive.mount('/content/drive')
+
 # Importing all required tools
 import tensorflow as tf
+import tensorflow_hub as hub
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
+from IPython.display import Image
+
+#@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
 
 print("TF Version",tf.__version__)
+print("TF hub version :",hub.__version__)
+# Checking for GPU accessibility
+
+print("GPU available"if tf.config.list_physical_devices("GPU")else "Not Available")
+print('*'*100)
+
+# To check if the image is accessible run this if an error occurs to know the root cause
+# !ls '/content/drive/MyDrive/Deepfake/Ai vs Real image Detection Dataset/train/FAKE/1069 (10).jpg'
+# Can be used for displaying images
+Image('/content/drive/MyDrive/Deepfake/Ai vs Real image Detection Dataset/train/FAKE/1069 (10).jpg')
+
+
+
+
