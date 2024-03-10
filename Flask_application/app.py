bd116547c8ce027918da18c8609ff1069d663200
vrmaverick <119831329+vrmaverick@users.noreply.github.com>
2024-03-11 00:25:28 +0530

    Update app.py

--- Flask_application/app.py
+++ Flask_application/app.py
@@ -2,7 +2,7 @@ from flask import Flask, flash , render_template, request, redirect, url_for , s
 import numpy as np
 import tensorflow as tf
 from werkzeug.utils import secure_filename
-from PIL import Image
+from PIL import Image ,ImageDraw,ImageFont
 import os
 
 app = Flask(__name__)
@@ -10,7 +10,8 @@ UPLOAD_FOLDER = 'static/uploads'
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 os.makedirs(UPLOAD_FOLDER, exist_ok=True)
 MODEL_PATH = r'my_model_1.h5'
-app.secret_key = "sex key"
+# MODEL_PATH = r'C://Users//vedant//Downloads//DF//DeepFake_Detector-main//Flask_application//my_model_1.h5'
+app.secret_key = "Argentavious"
 # Load the pre-trained model
 model = new_model = tf.keras.models.load_model(MODEL_PATH)
 
@@ -34,22 +35,44 @@ def portfolio():
 def contact():
     return render_template('contact.html')
 
+# @app.route('/upload', methods=['POST'])
+# def upload_file():
+#     os.makedirs(UPLOAD_FOLDER, exist_ok=True)
+#     uploaded_file = request.files['file']
+#     filename = secure_filename(uploaded_file.filename)
+#     print(filename)
+#     if uploaded_file.filename != '':
+#         file_path = os.path.join(UPLOAD_FOLDER, uploaded_file.filename)
+#         img_path = os.path.join('static/images', uploaded_file.filename)
+#         uploaded_file.save(file_path)
+#         uploaded_file.save(img_path)
+        
+#         flash('Image successfully uploaded')
+#         prediction = process_image(file_path)
+#         return render_template('uploaded_file.html', filename=filename,prediction=prediction)
+#     return render_template('index.html')
+# 
 @app.route('/upload', methods=['POST'])
 def upload_file():
     os.makedirs(UPLOAD_FOLDER, exist_ok=True)
     uploaded_file = request.files['file']
     filename = secure_filename(uploaded_file.filename)
-    print(filename)
     if uploaded_file.filename != '':
-        file_path = os.path.join(UPLOAD_FOLDER, uploaded_file.filename)
-        img_path = os.path.join('static/images', uploaded_file.filename)
+        file_path = os.path.join(UPLOAD_FOLDER, filename)
+        img_path = os.path.join('static/images', filename)
         uploaded_file.save(file_path)
         uploaded_file.save(img_path)
         
         flash('Image successfully uploaded')
         prediction = process_image(file_path)
-        return render_template('uploaded_file.html', filename=filename,prediction=prediction)
-    return render_template('index.html')
+        # Apply watermark
+        wm_image_path = apply_watermark(file_path, prediction)
+        
+        return render_template('uploaded_file.html', filename=filename, prediction=prediction, wm_image_path=wm_image_path)
+    
+    else:
+        flash('Invalid file format')
+        return render_template('index.html')
 
 # @app.route('/uploads/<filename>')
 # def uploaded_file(filename):
@@ -85,6 +108,64 @@ def process_image(file_path):
     prediction_label = f'{prediction_class}'
 
     return prediction_label
+
+def apply_watermark(input_image_path, watermark_text):
+    # Open the input image
+    image = Image.open(input_image_path)
+    # Apply watermark
+    draw = ImageDraw.Draw(image)
+    w  = image.size[0]
+    h = image.size[1]
+    # Define the position to place the text (top left corner)
+    text_x = 0
+    text_y = 0
+    
+    # Define the size of the rectangle
+    rectangle_width = w/4  # Adjust as needed
+    rectangle_height = 40  # Adjust as needed
+    
+    # Draw the semi-transparent black container at the top left corner
+    rectangle_x = text_x
+    rectangle_y = text_y
+    draw.rectangle([rectangle_x, rectangle_y, rectangle_x + rectangle_width, rectangle_y + rectangle_height], fill=(0, 0, 0, 50))
+
+    font = ImageFont.truetype("arial.ttf", 30)
+    draw.text((10, 10), watermark_text, fill=(255, 255, 255, 128), font=font)
+    
+    # Save the watermarked image
+    wm_image_path = os.path.join(os.path.dirname(input_image_path), "watermarked_" + os.path.basename(input_image_path))
+    image.save(wm_image_path)
+    
+    return wm_image_path
+# def apply_watermark(input_image_path, watermark_text):
+#     # Open the input image
+#     image = Image.open(input_image_path)
+    
+#     # Apply watermark
+#     draw = ImageDraw.Draw(image)
+#     width, height = image.size
+#     font = ImageFont.truetype("arial.ttf", 30)
+#     text_width, text_height = draw.textsize(watermark_text, font=font)
+    
+#     # Define the rectangle for the semi-transparent black container
+#     rectangle_width = text_width + 20
+#     rectangle_height = text_height + 10
+#     rectangle_x = width - rectangle_width - 10
+#     rectangle_y = height - rectangle_height - 10
+    
+#     # Draw the semi-transparent black container
+#     draw.rectangle([rectangle_x, rectangle_y, width, height], fill=(0, 0, 0, 128))
+    
+#     # Draw the text on top of the container
+#     text_x = rectangle_x + 10
+#     text_y = rectangle_y + 5
+#     draw.text((text_x, text_y), watermark_text, fill=(255, 255, 255), font=font)
+    
+#     # Save the watermarked image
+#     wm_image_path = os.path.join(os.path.dirname(input_image_path), "watermarked_" + os.path.basename(input_image_path))
+#     image.save(wm_image_path)
+    
+#     return wm_image_path
 
 if __name__ == '__main__':
     app.run(debug=True)
